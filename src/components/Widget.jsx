4ec2cab317c2e5f707bd446c2f6192c211cd404d
sauvik <sauvik@logic-square.com>
2025-01-01 13:32:05 +0530

    initial setup

---
+++ src/components/Widget.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./ui/button";
+
+const Widget = () => {
+  return (
+    <div className="fixed bottom-4 right-4 z-50">
+      <Button>Feedback</Button>
+    </div>
+  );
+};
+
+export default Widget;
